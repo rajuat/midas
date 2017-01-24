bc6b66c9ea4bb483bcc02c39987d4c982f732fd6
raju.athokpam <arc783bye875#>
2017-01-24 19:29:30 +0530

    push

--- scripts/index.js
+++ scripts/index.js
@@ -9,7 +9,7 @@ firebase.initializeApp(config);
 var storage = firebase.storage();
 var database = firebase.database();
 
-var app = angular.module('MyApp', ['ngMaterial', 'ngRoute', 'ngMessages', 'firebase', 'angular.filter', 'ngFileUpload', 'mailchimp']);
+var app = angular.module('MyApp', ['ngMaterial', 'ngRoute', 'ngMessages', 'firebase', 'angular.filter', 'ngFileUpload', 'mailchimp', 'ngSanitize']);
 
 app.config(function($mdThemingProvider) {
     $mdThemingProvider.theme('default')
@@ -57,7 +57,7 @@ app.config(function($routeProvider) {
 
 app.controller("base64Ctrl", function($scope, $firebaseArray) {
     var _validFileExtensions = [".jpg", ".jpeg", ".bmp", ".gif", ".png"];
-    var activitiesRef = firebase.database().ref('activities');
+    var activitiesRef = firebase.database().ref('contents');
     $scope.uploadFile = function() {
         //var ref = firebase.database().ref('activities');
         $scope.activities = $firebaseArray(activitiesRef);
@@ -111,38 +111,38 @@ app.controller("base64Ctrl", function($scope, $firebaseArray) {
     }
 });
 
-app.controller("AppCtrl", ["$scope", "$firebaseAuth", "Upload", function($scope, $firebaseAuth, Upload) {
+app.controller("LoginCtrl", ["$scope", "$firebaseAuth", "Upload", function($scope, $firebaseAuth, Upload) {
     $scope.imagePath = '../images/people.jpg';
     $scope.sorryImagePath = '../images/sorry.jpg';
     $scope.authObj = $firebaseAuth();
-        $scope.signin = function() {
-            $scope.authObj.$signInWithPopup("google").then(function(authData) {
-                $scope.userInfo = authData;
-                console.log("Signin in as:", authData);
-            }).catch(function(error) {
-                $scope.userInfo = undefined;
-                console.error("Signin failed:", error);
-            });
-        };
-
-        $scope.authObj.$onAuthStateChanged(function(authData) {
-            if (authData) {
-                $scope.userInfo = authData;
-                console.log("AuthData in as:", authData);
-            } else {
-                $scope.userInfo = undefined;
-                console.log("AuthData out");
-            }
+    $scope.signin = function() {
+        $scope.authObj.$signInWithPopup("google").then(function(authData) {
+            $scope.userInfo = authData;
+            console.log("Signin in as:", authData);
+        }).catch(function(error) {
+            $scope.userInfo = undefined;
+            console.error("Signin failed:", error);
         });
+    };
 
-        $scope.signout = function() {
-            firebase.auth().signOut().then(function() {
-                $scope.authData = undefined;
-                $scope.$apply();
-            }, function() {
-                console.log("Error while signing out!");
-            });
-        };
+    $scope.authObj.$onAuthStateChanged(function(authData) {
+        if (authData) {
+            $scope.userInfo = authData;
+            console.log("AuthData in as:", authData);
+        } else {
+            $scope.userInfo = undefined;
+            console.log("AuthData out");
+        }
+    });
+
+    $scope.signout = function() {
+        firebase.auth().signOut().then(function() {
+            $scope.authData = undefined;
+            $scope.$apply();
+        }, function() {
+            console.log("Error while signing out!");
+        });
+    };
 }]);
 
 app.controller("ActCtrl", function($scope, $firebaseArray) {
@@ -150,49 +150,177 @@ app.controller("ActCtrl", function($scope, $firebaseArray) {
     $scope.activities = $firebaseArray(ref);
 });
 
-app.controller("TeamCtrl", function($scope, $firebaseObject) {
+app.controller("VisionCtrl", function($scope, $firebaseObject) {
+
+});
+
+app.controller("AppCtrl", function($scope, $firebaseObject, $sce) {
+    //teams
     var dref = database.ref('teams/debkumari');
     dref.off();
     dref.once('value', function(snapshot) {
-        $scope.debkumariText = snapshot.val();
+        $scope.debkumariText = $sce.trustAsHtml(snapshot.val());
     });
-   /* $firebaseObject(database.ref('teams/debkumari')).$loaded().then(function(data) {
-        $scope.debkumariText = data.$value.tostring();
-      }).catch(function(error) {console.error("Error:", error);});*/
+    /* $firebaseObject(database.ref('teams/debkumari')).$loaded().then(function(data) {
+         $scope.debkumariText = data.$value.tostring();
+       }).catch(function(error) {console.error("Error:", error);});*/
     $firebaseObject(database.ref('teams/debchandra')).$loaded().then(function(data) {
         $scope.debchandraText = data.$value;
-      }).catch(function(error) {console.error("Error:", error);});
+    }).catch(function(error) {
+        console.error("Error:", error);
+    });
     $firebaseObject(database.ref('teams/james')).$loaded().then(function(data) {
         $scope.jamesText = data.$value;
-      }).catch(function(error) {console.error("Error:", error);});
+    }).catch(function(error) {
+        console.error("Error:", error);
+    });
     $firebaseObject(database.ref('teams/joy')).$loaded().then(function(data) {
         $scope.joyText = data.$value;
-      }).catch(function(error) {console.error("Error:", error);});
+    }).catch(function(error) {
+        console.error("Error:", error);
+    });
     $firebaseObject(database.ref('teams/kunan')).$loaded().then(function(data) {
         $scope.kunanText = data.$value;
-      }).catch(function(error) {console.error("Error:", error);});
+    }).catch(function(error) {
+        console.error("Error:", error);
+    });
     $firebaseObject(database.ref('teams/reuben')).$loaded().then(function(data) {
         $scope.reubenText = data.$value;
-      }).catch(function(error) {console.error("Error:", error);});
+    }).catch(function(error) {
+        console.error("Error:", error);
+    });
 
-    var storage = firebase.storage();
+
     storage.ref('teamsimage/debkumari.jpg').getDownloadURL().then(function(url) {
-      $scope.debkumariImg = url;
-    }).catch(function(error) {console.log('Problem downloading teamsimage/debkumari.jpg')});
+        $scope.debkumariImg = url;
+    }).catch(function(error) {
+        console.log('Problem downloading teamsimage/debkumari.jpg')
+    });
     storage.ref('teamsimage/debchandra.jpg').getDownloadURL().then(function(url) {
-      $scope.debchandraImg = url;
-    }).catch(function(error) {console.log('Problem downloading teamsimage/debchandra.jpg')});
+        $scope.debchandraImg = url;
+    }).catch(function(error) {
+        console.log('Problem downloading teamsimage/debchandra.jpg')
+    });
     storage.ref('teamsimage/james.jpg').getDownloadURL().then(function(url) {
-      $scope.jamesImg = url;
-    }).catch(function(error) {console.log('Problem downloading teamsimage/james.jpg')});
+        $scope.jamesImg = url;
+    }).catch(function(error) {
+        console.log('Problem downloading teamsimage/james.jpg')
+    });
     storage.ref('teamsimage/joy.jpg').getDownloadURL().then(function(url) {
-      $scope.joyImg = url;
-    }).catch(function(error) {console.log('Problem downloading teamsimage/joy.jpg')});
+        $scope.joyImg = url;
+    }).catch(function(error) {
+        console.log('Problem downloading teamsimage/joy.jpg')
+    });
     storage.ref('teamsimage/kunan.jpg').getDownloadURL().then(function(url) {
-      $scope.kunanImg = url;
-    }).catch(function(error) {console.log('Problem downloading teamsimage/kunan.jpg')});
+        $scope.kunanImg = url;
+    }).catch(function(error) {
+        console.log('Problem downloading teamsimage/kunan.jpg')
+    });
     storage.ref('teamsimage/reuben.jpg').getDownloadURL().then(function(url) {
-      $scope.reubenImg = url;
-    }).catch(function(error) {console.log('Problem downloading teamsimage/reuben.jpg')});
+        $scope.reubenImg = url;
+    }).catch(function(error) {
+        console.log('Problem downloading teamsimage/reuben.jpg')
+    });
+    storage.ref('teamsimage/sonia.jpg').getDownloadURL().then(function(url) {
+        $scope.soniaImg = url;
+    }).catch(function(error) {
+        console.log('Problem downloading teamsimage/sonia.jpg')
+    });
+    storage.ref('teamsimage/subha.jpg').getDownloadURL().then(function(url) {
+        $scope.subhaImg = url;
+    }).catch(function(error) {
+        console.log('Problem downloading teamsimage/subha.jpg')
+    });
+
+    //vision
+    storage.ref('vision/vision1.jpg').getDownloadURL().then(function(url) {
+        $scope.vision1Img = url;
+    }).catch(function(error) {
+        console.log('Problem downloading teamsimage/vision1.jpg')
+    });
+    storage.ref('vision/vision2.jpg').getDownloadURL().then(function(url) {
+        $scope.vision2Img = url;
+    }).catch(function(error) {
+        console.log('Problem downloading teamsimage/vision2.jpg')
+    });
+
+    //what we do
+    //Medicinal and Aromatic plants
+    storage.ref('turmeric.jpg').getDownloadURL().then(function(url) {
+        $scope.turmericImg = url;
+    }).catch(function(error) {
+        console.log('Problem downloading turmeric.jpg')
+    });
+
+    //pamel
+    storage.ref('pamel.jpg').getDownloadURL().then(function(url) {
+        $scope.pamelImg = url;
+    }).catch(function(error) {
+        console.log('Problem downloading pamel.jpg')
+    });
+
+    //organicfarming
+    storage.ref('organicfarming2.jpg').getDownloadURL().then(function(url) {
+        $scope.organicfarming2Img = url;
+    }).catch(function(error) {
+        console.log('Problem downloading organicfarming2.jpg')
+    });
+    $firebaseObject(database.ref('contents/organicfarming')).$loaded().then(function(data) {
+        $scope.organicfarmingText = data.$value;
+    }).catch(function(error) {
+        console.error("Error:", error);
+    });
+
+    //traditional knowledge
+    storage.ref('traditonalknowledge1.jpg').getDownloadURL().then(function(url) {
+        $scope.traditonalknowledge1Img = url;
+    }).catch(function(error) {
+        console.log('Problem downloading traditonalknowledge1.jpg')
+    });
+    $firebaseObject(database.ref('contents/traditionalknowledge')).$loaded().then(function(data) {
+        $scope.traditionalknowledgeText = data.$value;
+    }).catch(function(error) {
+        console.error("Error:", error);
+    });
+
+    //climate change
+    storage.ref('loktakhomepage.jpg').getDownloadURL().then(function(url) {
+        $scope.loktakhomepageImg = url;
+    }).catch(function(error) {
+        console.log('Problem downloading loktakhomepage.jpg')
+    });
+    storage.ref('djuko.jpg').getDownloadURL().then(function(url) {
+        $scope.djukoImg = url;
+    }).catch(function(error) {
+        console.log('Problem downloading djuko.jpg')
+    });
+    storage.ref('nongleinungshit.jpg').getDownloadURL().then(function(url) {
+        $scope.nongleinungshitImg = url;
+    }).catch(function(error) {
+        console.log('Problem downloading nongleinungshit.jpg')
+    });
+    $firebaseObject(database.ref('contents/climatechange')).$loaded().then(function(data) {
+        $scope.climatechangeText = data.$value;
+    }).catch(function(error) {
+        console.error("Error:", error);
+    });
+
+    //herbal extraction
+    $firebaseObject(database.ref('contents/herbalextraction')).$loaded().then(function(data) {
+        $scope.herbalextractionText = data.$value;
+    }).catch(function(error) {
+        console.error("Error:", error);
+    });
+
+    //urbangardening
+    $firebaseObject(database.ref('contents/urbangardening')).$loaded().then(function(data) {
+        $scope.urbangardeningText = data.$value;
+    }).catch(function(error) {
+        console.error("Error:", error);
+    });
+
+});
+
+app.controller("WhatWeDoCtrl", function($scope, $firebaseObject) {
 
 });
